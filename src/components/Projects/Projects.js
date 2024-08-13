d47e89ae7e1f5daffec47fdc28a6a616a5f4c6bc
Glen Alloy <glenalloy8@gmail.com>
2024-08-13 11:36:32 +0530

    created project object array

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,32 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+const projectsDemo = [{
+  title: 'Project 1',
+  description: 'This is a really long project about this project'
+},{
+  title: 'Project 2',
+  description: 'This is a really long project about this project
+},{
+  title: 'Project 3',
+  description: 'This is a really long project about this project
+},{
+  title: 'Project 4',
+  description: 'This is a really long project about this project
+}];
+
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+      <SectionDivider/>
+      <SectionTitle main>Projects</SectionTitle>
+      <GridContainer>
+        {[0, 1, 2, 3, 4, 5].map((project => {
+          <div>
+            {project}
+          </div>
+        }))}
+      </GridContainer>
+  </Section>
 );
 
 export default Projects;
